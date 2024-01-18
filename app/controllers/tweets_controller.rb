5c541f7d90c42b2ebd71a854afbbe3f1d6dcc798
i-shintaro <s.iwashita@di-v.co.jp>
2024-01-19 07:17:09 +0900

    2024/01/19

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -17,10 +19,26 @@ class TweetsController < ApplicationController
     tweet.destroy
     redirect_to root_path
   end
-  
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end
 
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
